1de8239d69fb6e2a3c8d7299522addb277c8c98e
159357159357asdfghjkl <159357159357asdfghjkl@gmail.com>
2024-07-30 10:52:38 +0800

    a test to find bugs

--- source/mobile/flixel/FlxButton.hx
+++ source/mobile/flixel/FlxButton.hx
@@ -403,7 +403,7 @@ class FlxTypedButton<T:FlxSprite> extends FlxSprite implements IFlxInput
 		{
 			currentInput = null;
 		}
-		else if (overlapsPoint(pointer.getWorldPosition(camera, _point), true, camera))
+		else if (overlapsPoint(pointer.getWorldPosition(camera, _point), false/*true*/, camera))
 		{
 			updateStatus(input);
 			return true;
